fc8494388a3050c11ad897107c0f32899ab283fc
Sadaf Ghasemi <sadaf.ghasemi@otto.de>
2022-12-12 14:21:01 +0100

    complate games view

--- src/views/GamesView.vue
+++ src/views/GamesView.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="games">
         <h1>Comming Soon... &#x1F609;</h1>
+        <br><br><br>
+        <img alt="construction turtle" src="../assets/construction_turtle.jpg" height=500  width=400>
+
     </div>
 </template>
 
